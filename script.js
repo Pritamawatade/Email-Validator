336a2b2e19e6b16be9341feecb89523f4aabe0e9
pritamawatade <pritamawatade5@gmail.com>
2024-06-19 11:51:19 +0530

    UI is done

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 console.log("script is included");
-let result = 
-    {
-        "tag": "",
-        "free": true,
-        "role": false,
-        "user": "pritamawatadeluffy",
-        "email": "pritamawatadeluffy@gmail.com",
-        "score": 0.48,
-        "state": "undeliverable",
-        "domain": "gmail.com",
-        "reason": "invalid_mailbox",
-        "mx_found": true,
-        "catch_all": null,
-        "disposable": false,
-        "smtp_check": false,
-        "did_you_mean": "",
-        "format_valid": true
-      }
+let result =
+{
+  "tag": "",
+  "free": true,
+  "role": false,
+  "user": "pritamawatadeluffy",
+  "email": "pritamawatadeluffy@gmail.com",
+  "score": 0.48,
+  "state": "undeliverable",
+  "domain": "gmail.com",
+  "reason": "invalid_mailbox",
+  "mx_found": true,
+  "catch_all": null,
+  "disposable": false,
+  "smtp_check": false,
+  "did_you_mean": "",
+  "format_valid": true
+};
+let str = ``
 
-      let str = ``
-      for (key of Object.keys(result))
-        {
-            str = str + `<div>${key}: ${result[key]}</div>`;
-        }
-
-
-
+for (key of Object.keys(result)) {
+  str = str + `<div>${key}: ${result[key]}</div>`;
+}
 
 console.log(str)
+resultCont.innerHTML = str;
 
